a4ea4e60232f10d73fbd4eaf0f8a3b695313142d
Bownuz <fedde.snelder@gmail.com>
2024-05-30 12:37:32 +0200

    taskbar updated. Now had a backbutton and questionbutton (questionbutton doesn't have a image)

--- androidCode/app/src/main/java/com/example/mobielebeleving/activityclasses/ChooseLocationActivity.java
+++ androidCode/app/src/main/java/com/example/mobielebeleving/activityclasses/ChooseLocationActivity.java
@@ -1,7 +1,11 @@
 package com.example.mobielebeleving.activityclasses;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import androidx.recyclerview.widget.RecyclerView;
@@ -46,5 +50,35 @@ public class ChooseLocationActivity extends AppCompatActivity
         Intent intent = new Intent(this, EnterCodeActivity.class);
         intent.putExtra(EnterCodeActivity.EXTRA_INFO_ID, position);
         startActivity(intent);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.helpButton) {
+            AlertDialog dialog = createDialog();
+            dialog.show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    AlertDialog createDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Uitleg");
+        builder.setMessage(R.string.helpDialog);
+        builder.setPositiveButton("BEGREPEN", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        return builder.create();
     }
 }
