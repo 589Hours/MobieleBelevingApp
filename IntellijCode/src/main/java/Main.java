4723ee89dac3cc8bb35b67dddae914b1fe5d4042
Rick <rrhvanhoof@gmail.com>
2024-06-02 20:21:41 +0200

    Server setup
    het begin van de server. Library van de MQTT connectie werkt niet

---
+++ IntellijCode/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Main {
+    private static ArrayList<Sensor> sensors = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serversocket = new ServerSocket(12345);
+
+
+        while(true) {
+            Socket socket = serversocket.accept();
+            sensors.add(new Sensor(socket, sensors.size()+1));
+        }
+    }
+}
---
+++ IntellijCode/src/main/java/Sensor.java
@@ -0,0 +1,28 @@
+import java.net.Socket;
+//import org.eclipse.paho.client.mqttv3.MqttClient;
+//import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+//import org.eclipse.paho.client.mqttv3.MqttException;
+//import org.eclipse.paho.client.mqttv3.MqttMessage;
+//import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class Sensor {
+    private boolean isOn;
+    private String topic;
+    private final Socket socket;
+
+    public Sensor(Socket socket, int sensorNumber) {
+        this.isOn = false;
+        this.topic = "avanstibreda/ti/1.4/a6/s" + sensorNumber;
+        this.socket = socket;
+
+        Thread t = new Thread(this::ReadData);
+        t.start();
+    }
+
+    public void ReadData() {
+
+        while(socket.isConnected()) {
+//            MqttClient client
+        }
+    }
+}
