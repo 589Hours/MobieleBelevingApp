d0cd8369d130884671afa220156c31c5e345631b
Falcon.G2 <ctw.vanderpluijm@student.avans.nl>
2024-06-04 10:13:11 +0200

    Talen weg uit toolbar

--- androidCode/app/src/main/java/com/example/mobielebeleving/activityclasses/MainActivity.java
+++ androidCode/app/src/main/java/com/example/mobielebeleving/activityclasses/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -43,8 +44,30 @@ public class MainActivity extends AppCompatActivity {
               navigateChooseLocationActivity();
             }
         });
-//        ImageButton dutch = findViewById(R.id.dutchButton);
+
+
+        ImageButton dutch = findViewById(R.id.dutchButton);
+        ImageButton english = findViewById(R.id.englishButton);
         ImageButton info = findViewById(R.id.helpButton);
+
+        LanguageManager lm = new LanguageManager(this);
+
+        dutch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lm.updateResources("nl");
+                Toast toast = Toast.makeText(MainActivity.this,"Geselecteerde taal: Nederlands!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        english.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lm.updateResources("en");
+                Toast toast = Toast.makeText(MainActivity.this,"Selected language: English!",Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
         info.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -55,9 +78,9 @@ public class MainActivity extends AppCompatActivity {
     }
     AlertDialog createDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Uitleg");
+        builder.setTitle(R.string.helpTitel);
         builder.setMessage(R.string.helpDialog);
-        builder.setPositiveButton("BEGREPEN", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(R.string.roger, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
